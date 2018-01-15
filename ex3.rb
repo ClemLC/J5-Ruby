cee2b0a0b50da800c69e5daebcc8fb90f6e68007
Clement <clementlecuillier@hotmail.fr>
2018-01-15 18:18:27 +0100

    trader du dimanche fini

--- ex3.rb
+++ ex3.rb
@@ -1,24 +1,26 @@
 #def trader du dimanche
 
-def trader_du_dimanche(cours_action)
+def trader_du_dimanche(a)
 
-  for j in (0..a.length-1)do
-  for i in (O..a.length-1)do
-    if a[j]-a[i] > max &&j >i
+max = 0
+
+  for j in(0..a.length-1)do
+  for i in(0..a.length-1)do
+
+    if a[j]-a[i] > max && j>i
       max = a[j]-a[i]
       px_vente = a[j]
       px_achat = a[i]
-      j_vente = j
-      j_achat = i
-
-    else puts "error"
+      j_vente = j +1
+      j_achat = i +1
 
     end
   end
+end
 
-  puts "Jour d'achat #{J_achat}"
+  puts "Jour d'achat #{j_achat}"
   puts "Jour de vente #{j_vente}"
-  puts "P&L : #{max}$ !"
+  puts "P&L : #{max}"
 end
 
 trader_du_dimanche([6,17,4,22,8,44])
